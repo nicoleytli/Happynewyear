c07a22f59d2d920930064d76d606ec983176ab58
Yutong Li <nicole.yt.li@hotmail.com>
2017-01-24 19:31:24 +0100

    First edition

---
+++ Roulette.py
@@ -0,0 +1,37 @@
+import random
+# This is used to fix the random generator so we can test the output
+random.seed(3456)
+
+class Roulette:
+    def __init__(self, min):
+        self.min = min
+
+    def SimulateGame(self, bets, amounts):
+        def AboveMinimum(amounts):
+            result1 = []
+            for amount in amounts:
+                result1.append(bool(amount >= self.min))
+            return (result1)
+
+        def SpinTheWheel(bets):
+            print(" Spinning the wheel...")
+            truth = random.randint(0, 36)
+            result2 = []
+            number = 0
+            for bet in bets:
+                result2.append(bool(bet == truth))
+                if bet == truth:
+                    number += 1
+                else:
+                    continue
+            print("Ball lands on " + str(truth))
+
+            if number != 0:
+                print("There are " + str(number) + " correct bet(s)")
+            else:
+                print("No winners this round")
+            return (result2)
+
+        Playerwin = [i * j * k for i, j, k in zip(amounts, AboveMinimum(amounts), SpinTheWheel(bets))]
+        Casinowin = sum(amounts) - sum(Playerwin)
+        return [Casinowin, [i * 30 for i in Playerwin]]
