a5db853376b5e9148c8f35aacf101a6115e6779f
Yutong Li <nicole.yt.li@hotmail.com>
2017-01-25 22:29:33 +0100

    Second edition

--- Simulation.py
+++ Simulation.py
@@ -11,8 +11,8 @@ table1 = Roulette.Roulette(100)
 print(table1.SimulateGame(bets1, amounts1))
 print(table1.SimulateGame(bets1, amounts1))
 
-# amounts1 = [25, 85, 120, 65, 150, 122]
-# bets1 = [10, 24, 36, 0, 11, 24]
-# table1 = Craps.Craps(10)
-# print(table1.SimulateGame(bets1, amounts1))
-# print(table1.SimulateGame(bets1, amounts1))
+amounts1 = [5, 85, 120, 65, 150, 122]
+bets1 = [10, 4, 7, 11, 11, 3]
+table1 = Craps.Craps(10)
+print(table1.SimulateGame(bets1, amounts1))
+print(table1.SimulateGame(bets1, amounts1))
