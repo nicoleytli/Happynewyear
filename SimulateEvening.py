1684afb060d8f6cc5bf0139cc6b31612e27fcb79
Yutong Li <nicole.yt.li@hotmail.com>
2017-01-29 22:02:41 +0100

    Final Edition: added more comments

--- SimulateEvening.py
+++ SimulateEvening.py
@@ -2,6 +2,8 @@ import Evening
 from matplotlib import pyplot as plt
 
 cashflow = []
+
+# A function for simulating
 def Go(numroulette, numcraps, barman, wage, cash, total, returning, bachelor, freebudget, evenings):
 
     numroulette = numroulette
@@ -16,13 +18,15 @@ def Go(numroulette, numcraps, barman, wage, cash, total, returning, bachelor, fr
 
     for evening in range(evenings):
         print("Evening " + str(evening + 1))
-        cash, profit = Evening.SimulateEvening(numroulette, numcraps, barman, wage, cash, total, returning, bachelor, freebudget)
+        cash, profit, avgbtips, avgctips = Evening.SimulateEvening(numroulette, numcraps, barman, wage, cash, total, returning, bachelor, freebudget)
         cashflow.extend([profit])
 
+    # Plot the result
     plt.title('Cashflow of Casino')
     plt.ylabel('profit($)')
     plt.xlabel('Evening')
     plt.plot(range(1, len(cashflow) + 1), cashflow)
     plt.show()
 
+# Evening
 print(Go(10, 10, 4, 200, 50000, 100, 0.5, 0.1, 200, 1000))
